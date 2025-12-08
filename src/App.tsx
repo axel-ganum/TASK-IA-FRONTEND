33fc6be8512f23c28b0b42cb0926dbbaa1da0e59
axel-ganum <axel.gaum@gmail.com>
2025-12-08 11:58:34 -0300

    fix: corregir sintaxis en App.tsx para renderizado correcto

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@
 import { Home } from './pages/Home'
 
 function App() {
-  <div className="min-h-screen bg-[#0c0c0c] text-white">
-  return <Home /> 
-  </div>
-
-
-    
+  return (
+    <div className="min-h-screen bg-[#0c0c0c] text-white">
+      <Home /> 
+    </div>
+  )
 }
 
 export default App
